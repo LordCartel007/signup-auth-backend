2d3521ecf3c387213d39cdae9e902738d69f9894
LordCartel007 <cartellord77@gmail.com>
2025-05-07 04:48:50 +0100

    adjusted link again

--- utils/generateTokenAndSetCookie.js
+++ utils/generateTokenAndSetCookie.js
@@ -7,7 +7,7 @@ export const generateTokenAndSetCookie = (res, userId) => {
   });
   res.cookie("token", token, {
     httpOnly: true, // only accessible by the web server to prevent XSS attacks
-    secure: process.env.NODE_ENV === "production", // set to true if using HTTPS
+    secure: true, // set to true if using HTTPS
     sameSite: "strict", // helps prevent CSRF attacks
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
   });
