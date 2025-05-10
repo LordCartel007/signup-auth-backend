828375c5edcb04a3d297380ae1d0a303dd63b67f
LordCartel007 <cartellord77@gmail.com>
2025-05-10 17:51:56 +0100

    changed cors link

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://signup-frontend-zeta.vercel.app/homepage",
     credentials: true,
   })
 ); // to allow cross-origin requests
